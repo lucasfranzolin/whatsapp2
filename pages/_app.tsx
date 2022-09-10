63f7daf4b201013015802da8eb1418a193253f24
Lucas Franzolin <lucasfranzolin@gmail.com>
2022-09-10 16:10:30 -0300

    fix eslint import errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
-import { AppProps } from "next/app"
-import "tailwindcss/tailwind.css"
-import "@styles/globals.css"
+import 'tailwindcss/tailwind.css';
+import '@styles/globals.css';
+
+import { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
